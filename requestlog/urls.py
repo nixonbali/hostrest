510f25e5e3cdff4342e7007dd80264fe36200b1f
nixonbali <nixon.ubud@gmail.com>
2020-05-20 11:28:26 -0400

    adds requestlog-detail to urls - for hyperlinking

--- requestlog/urls.py
+++ requestlog/urls.py
@@ -6,6 +6,13 @@ request_list = RequestLogViewSet.as_view({
     'get': 'list'
 })
 
+request_detail = RequestLogViewSet.as_view({
+    'get': 'retrieve',
+    'post': 'update',
+    'delete': 'destroy'
+})
+
 urlpatterns = format_suffix_patterns([
-    path('', request_list, name="requestlog-list")
+    path('', request_list, name="requestlog-list"),
+    path('api/<int:pk>/', request_detail, name="requestlog-detail")
 ])
