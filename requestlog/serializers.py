d3c32904d0de7f2c081e5ce68c9e3cf0b31db68b
nixonbali <nixon.ubud@gmail.com>
2020-05-20 12:02:58 -0400

    sets requestlog field permissions - only comment writable

--- requestlog/serializers.py
+++ requestlog/serializers.py
@@ -5,3 +5,10 @@ class RequestLogSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = RequestLog
         fields = ['url', 'id', 'time', 'type', 'comment']
+        extra_kwargs = {
+            'url': {'read_only': True},
+            'id': {'read_only': True},
+            'time': {'read_only': True},
+            'type': {'read_only': True},
+            'comment': {'read_only': False}
+        }
